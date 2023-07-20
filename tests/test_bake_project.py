6e35a348d15b627c67d2a7be6ae1aac230901a7c
billsioros <billsioros97@gmail.com>
2023-07-20 17:40:00 +0300

    feat!: v1

---
+++ tests/test_bake_project.py
@@ -0,0 +1,96 @@
+import contextlib
+import datetime
+import os
+import shlex
+import subprocess
+
+import pytest
+from cookiecutter.utils import rmtree
+
+
+@contextlib.contextmanager
+def inside_dir(dirpath):
+    """Execute code from inside the given directory.
+
+    Args:
+        dirpath ([string]): path of the directory the command is being run.
+    """
+    old_path = os.getcwd()
+    try:
+        os.chdir(dirpath)
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+def run_inside_dir(command, dirpath):
+    """
+    Run a command from inside a given directory, returning the exit status
+    :param command: Command that will be executed
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    with inside_dir(dirpath):
+        return subprocess.check_call(shlex.split(command))
+
+
+@contextlib.contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
+    result = cookies.bake(*args, **kwargs)
+
+    try:
+        yield result
+    finally:
+        if result.exception is not None:
+            with contextlib.suppress(FileNotFoundError):
+                rmtree(str(result.project_path))
+
+
+def test_bake_with_defaults(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.exception is None
+        assert result.exit_code == 0
+        assert result.project_path is not None
+        assert result.project_path.is_dir()
+
+        found_toplevel_files = [f.name for f in result.project_path.iterdir()]
+        assert ".github" in found_toplevel_files
+        assert "docs" in found_toplevel_files
+        assert "src" in found_toplevel_files
+        assert "tests" in found_toplevel_files
+        assert ".editorconfig" in found_toplevel_files
+        assert ".gitignore" in found_toplevel_files
+        assert ".devcontainer" in found_toplevel_files
+        assert ".pre-commit-config.yaml" in found_toplevel_files
+        assert "CHANGELOG.md" in found_toplevel_files
+        assert "LICENSE" in found_toplevel_files
+        assert "mkdocs.yml" in found_toplevel_files
+        assert "pyproject.toml" in found_toplevel_files
+        assert "README.md" in found_toplevel_files
+
+
+def test_bake_withspecialchars(cookies):
+    """Ensure that a `project_name` with double quotes is invalid"""
+    with bake_in_temp_dir(cookies, extra_context={"project_name": 'name "quote" name'}) as result:
+        assert result.exit_code != 0
+        assert result.exception is not None
+
+
+def test_bake_with_apostrophe(cookies):
+    """Ensure that a `project_name` with apostrophes is invalid"""
+    with bake_in_temp_dir(cookies, extra_context={"project_name": "O'connor"}) as result:
+        assert result.exit_code != 0
+        assert result.exception is not None
+
+
+def test_year_compute_in_license_file(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        year = str(datetime.datetime.utcnow().year)
+
+        license_text = (result.project_path / "LICENSE").read_text()
+
+        assert year in license_text
---
+++ {{cookiecutter.project_name}}/README.md
@@ -0,0 +1,72 @@
+<h1 align="center">{{ cookiecutter.project_name.replace('-', ' ').title() }}</h1>
+
+<p align="center"><em>{{ cookiecutter.project_description }}</em></p>
+
+<p align="center">
+  <a href="{{ cookiecutter.github_repository }}/actions/workflows/ci.yml">
+    <img
+      src="{{ cookiecutter.github_repository }}/actions/workflows/ci.yml/badge.svg"
+      alt="CI"
+    />
+  </a>
+  <a href="{{ cookiecutter.github_repository }}/actions/workflows/cd.yml">
+    <img
+      src="{{ cookiecutter.github_repository }}/actions/workflows/cd.yml/badge.svg"
+      alt="CD"
+    />
+  </a>
+  <a href="https://results.pre-commit.ci/latest/github/{{cookiecutter.github_user}}/{{cookiecutter.project_name}}/master">
+    <img
+      src="https://results.pre-commit.ci/badge/github/{{cookiecutter.github_user}}/{{cookiecutter.project_name}}/master.svg"
+      alt="pre-commit.ci status"
+    />
+  </a>
+  <a href="https://codecov.io/gh/{{cookiecutter.github_user}}/{{cookiecutter.project_name}}">
+    <img
+      src="https://codecov.io/gh/{{cookiecutter.github_user}}/{{cookiecutter.project_name}}/branch/master/graph/badge.svg?token=coLOL0j6Ap"
+      alt="Test Coverage"/>
+  </a>
+  <a href="https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url={{ cookiecutter.github_repository }}">
+    <img
+      src="https://img.shields.io/static/v1?label=Dev%20Containers&message=Open&color=blue&logo=visualstudiocode"
+      alt="Open in GitHub Codespaces"
+    />
+  </a>
+  <a href="https://github.com/Group4Life/cookiecutter-pypackage">
+    <img
+      src="https://img.shields.io/badge/cookiecutter-template-D4AA00.svg?style=flat&logo=cookiecutter"
+      alt="Cookiecutter Template">
+  </a>
+  <a href="https://app.renovatebot.com/dashboard#github/{{cookiecutter.github_user}}/{{cookiecutter.project_name}}">
+    <img
+      src="https://img.shields.io/badge/renovate-enabled-brightgreen.svg?style=flat&logo=renovatebot"
+      alt="Renovate - Enabled">
+  </a>
+</p>
+
+## :cd: Installation
+
+In order to locally set up the project please follow the instructions below:
+
+```shell
+# Set up the GitHub repository
+git init
+git config --local user.name {{cookiecutter.author}}
+git config --local user.email {{cookiecutter.email}}
+git add .
+git commit -m "feat: initial commit"
+git remote add origin {{cookiecutter.github_repository}}
+
+# Create a virtual environment using poetry and install the required dependencies
+poetry shell
+poetry install
+
+# Install pre-commit hooks
+pre-commit install --install-hooks
+```
+
+## :sparkles: Contributing
+
+Before contributing to the project, please go through the [Contributing Guidelines](https://{{cookiecutter.github_user}}.github.io/{{cookiecutter.project_name}}/latest/CONTRIBUTING/) first.
+
+The project's documentation can be found [here](https://{{cookiecutter.github_user}}.github.io/{{cookiecutter.project_name}}/).
